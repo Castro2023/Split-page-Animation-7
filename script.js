d705df3cf116a7687f2f30fd85dffff721527d6e
Castro2023 <charlesugwoke93@gmail.com>
2023-11-01 23:20:35 +0100

    initial comit

---
+++ script.js
@@ -0,0 +1,25 @@
+const container=document.querySelector('.container');
+const left= document.querySelector('.left');
+const right= document.querySelector('.right');
+
+
+// left.addEventListener('mouseenter', ()=>{
+//     container.classList.toggle('hover-left') 
+// })
+// right.addEventListener('mouseenter', ()=>{
+//     container.classList.toggle('hover-right') 
+// })
+
+left.addEventListener('mouseenter',()=>{
+    container.classList.add('hover-left')
+})
+left.addEventListener('mouseleave',()=>{
+    container.classList.remove('hover-left')
+})
+
+right.addEventListener('mouseenter',()=>{
+    container.classList.add('hover-right')
+})
+right.addEventListener('mouseleave',()=>{
+    container.classList.remove('hover-right')
+})
